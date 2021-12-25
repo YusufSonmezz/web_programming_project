b05fbac7d60a253dd0407ffae754e9624d7dd9d4
YusufSonmezz <yusufsn54@gmail.com>
2021-12-25 03:41:41 +0300

    Last_Changes

--- WebProgProje/Controllers/ArticleApiController.cs
+++ WebProgProje/Controllers/ArticleApiController.cs
@@ -30,27 +30,45 @@ namespace WebProgProje.Controllers
 
         // GET api/<ArticleApiController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Article Get(int id)
         {
-            return "value";
+            var the_article = paperContext.Articles.Where(article => article.ArticleId == id).FirstOrDefault();
+            return the_article;
         }
 
         // POST api/<ArticleApiController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Article article)
         {
+            Article articleAdding = article;
+            paperContext.Articles.Add(articleAdding);
+            paperContext.SaveChanges();
+
         }
 
         // PUT api/<ArticleApiController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Article article)
         {
+            Article theArticle = paperContext.Articles.Where(article => article.ArticleId == id).FirstOrDefault();
+
+            theArticle.Comments = article.Comments;
+            theArticle.Content = article.Content;
+            theArticle.DateOfAdding = article.DateOfAdding;
+            theArticle.Description = article.Description;
+            theArticle.Image = article.Image;
+            theArticle.Title = article.Title;
+
+            paperContext.SaveChanges();
         }
 
         // DELETE api/<ArticleApiController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            Article theArticle = paperContext.Articles.Where(article => article.ArticleId == id).FirstOrDefault();
+            paperContext.Articles.Remove(theArticle);
+            paperContext.SaveChanges();
         }
     }
 }
